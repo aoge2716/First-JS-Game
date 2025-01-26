f5b778bdebda7aa9e93cde0a2e6af34dec8d6acd
aoge2716 <aoge2716@gmail.com>
2025-01-26 22:38:35 +0100

    added new method and generalised the constructor

--- src/rod.js
+++ src/rod.js
@@ -1,58 +1,103 @@
 class Ball{
-    constructor(){
-        this.width = 200;
-        this.height = 50; 
-        this.positionX = 19; 
-        this.positionY = 80;
-
-        this.ballElm = document.querySelector("#ball");
-        this.rodElm = document.querySelector("#rod");
+    constructor(elmClass, bar, pin){
+        // this.ballElm = document.querySelector("#ball");
+        // this.rodElm = document.querySelector("#rod");
+        this.rodElm = this.create(bar,document.querySelector(elmClass));
+        this.ballElm = this.create(pin, this.rodElm);
         
-        this.addimg(this.rodElm, "../data/flag-pole-bar-free-png.webp");
-        // this.addimg(this.rodElm, "data/Jose.jpg");
-        this.addimg(this.ballElm, "../data/fist.webp");
-        
+        // get the position of rod Body
+        this.rodBody = this.rodElm.getBoundingClientRect();
+        console.log("body: ",this.rodBody)
+        console.log("ball width: ", this.ballElm.offsetWidth);
 
-        this.draw();
+        // get the percentage of the center bottom of the main bar
+        this.initX = Math.round(((this.rodBody.width/2 - (this.ballElm.offsetWidth/2))/this.rodBody.width*100)*100)/100;
+        this.initY = Math.round(((this.rodBody.height - this.ballElm.offsetHeight-1)/this.rodBody.height*100)*100)/100; 
+        this.positionX =  this.initX; 
+        this.positionY = this.initY;
+        console.log("positionX: ",this.positionX);
+        console.log("positionY: "+ this.positionY+"%") // Bottom inside the rod
+
+        this.draw(); 
     }
-    addimg(target, src){
-        const ballimg = document.createElement("img");
-        ballimg.src = src;
-        ballimg.alt = "Ball";
-        ballimg.style.width = "100%";
-        target.appendChild(ballimg)
+
+    create(elmClass, parent){
+        // create element with given class and append to given parent 
+        const el = document.createElement("div");
+        el.classList.add(elmClass);
+        parent.appendChild(el);
+        console.log(parent, el);
+        return el;
+        
+    }
+
+    addimg(target, src, altdesc){
+        const img = document.createElement("img");
+        img.src = src;
+        img.alt = altdesc;
+        img.style.width = "100%";
+        target.appendChild(img)
 
     }
 
     draw(){
-        this.ballElm.style.left = this.positionX + "vw";
-        this.ballElm.style.top = this.positionY + "vh";
-
-        this.ballElm.style.width = this.width + "px";
-        this.ballElm.style.height = this.height + "px";
+        this.ballElm.style.left = this.positionX + "%";
+        this.ballElm.style.top = this.positionY + "%";
     }
 
     moveLeft(){
-        this.positionX--;
-        console.log(this.positionX)
+        this.positionX-=10;
+        console.log(this.positionX,this.positionY);
         this.draw();
     }
     
     moveRight(){
-        this.positionX++;
+        this.positionX+=10;
+        console.log(this.positionY);
         this.draw();
     }
 
-    moveDown(counter){
+    moveDown(){
         
-        this.positionY+=2;
-        // console.log(this.positionY)
+        this.positionY++;
+        console.log(this.positionY);
         this.draw();
     }
 
-    moveUp(counter){
-        this.positionY-=2;
+    moveUp(){
+        this.positionY--;
+        console.log(this.positionY);
         this.draw();
     }
+
+    hit(max,speed,stop){
+        if(!intrvl){
+            intrvl = setInterval(() => {
+                if(goingUp){
+                        if (this.positionY < 100- max){
+                            console.log("passed")
+                            goingUp = false;
+                            
+                        }else{
+                            this.moveUp();
+                        }
+                }else{
+                    if(this.positionY == this.initY){
+                        goingUp = true;
+                        if(stop){
+                            clearInterval(intrvl);
+                            window.alert(max+"%");
+                        }
+                        // clearInterval(intrvl);
+                        intrvl = null;
+                    }else{
+                        this.moveDown();
+                    }
+                }   
+            }, speed);
+        }     
+    }
+
+    
 }
 
